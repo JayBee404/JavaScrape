0f7bcaa745a7013ba1c926b2f08d967969e03a17
JayBee404 <156393141+JayBee404@users.noreply.github.com>
2024-07-31 03:17:46 -0400

    Add files via upload

---
+++ src/MineLinks.java
@@ -0,0 +1,100 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+public class MineLinks extends Application
+{
+    private TextArea outArea;
+    private Button doItButton;
+    private Label urlLabel;
+    private TextField urlText;
+
+    private String textLine = new String("");
+    private String outString = new String();
+    
+    private final String initialString = ""; //enter URL to scrape here
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        outArea = new TextArea();
+        outArea.setPrefColumnCount(50);
+        outArea.setPrefRowCount(40);
+        outArea.setWrapText(false);
+        outArea.setFont(Font.font("Courier New", FontWeight.BOLD, 20));
+
+        urlLabel = new Label("URL: ");
+        urlText = new TextField();
+        urlText.setPrefWidth(400);
+        urlText.setText(initialString);
+
+        doItButton    = new Button("Get HTML Doc");
+        doItButton.setOnAction(new ButtonClickHandler());
+
+        HBox buttonBox = new HBox(15,urlLabel,urlText,doItButton);
+        buttonBox.setAlignment(Pos.CENTER);
+        VBox mainBox = new VBox(10,buttonBox,outArea);
+        mainBox.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(mainBox, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("URL HTML Doc Getter");
+        primaryStage.show();
+    }
+
+    class ButtonClickHandler implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event)
+        {
+            try
+            {
+                URL theURL = new URL(urlText.getText());
+
+                BufferedReader inputStream = new BufferedReader(
+                        new InputStreamReader(theURL.openStream()));
+
+                String inputLine;
+                String webpage = "";
+                while ((inputLine = inputStream.readLine()) != null)
+                {
+                    webpage += inputLine;
+                }
+                inputStream.close();
+
+                try {
+                    FileWriter fileWriter = new FileWriter("");// output filename here
+                    fileWriter.write(webpage);
+                    fileWriter.close();
+                    System.out.println("Successfully wrote to the file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred while writing to the file.");
+                    e.printStackTrace();
+                }
+
+            }
+            catch (IOException exception)
+            {
+                exception.printStackTrace();
+
+            }
+        }
+    }
+}
---
+++ src/parseHTML.java
@@ -0,0 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class parseHTML {
+    public static void main(String[] args) {
+
+        String fileName = ""; // input filename here
+        String fileName2 = ""; // output filename here
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));
+             FileWriter fileWriter = new FileWriter(fileName2)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String pattern = "style=\"text-align:left\">\"(.*?)\"<"; // adjust regex pattern as necessary
+
+                Pattern regexPattern = Pattern.compile(pattern);
+
+                Matcher matcher = regexPattern.matcher(line);
+
+                while (matcher.find()) {
+                    String title = matcher.group(1);
+                    fileWriter.write(title.toLowerCase() + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading from the file or writing to the file: " + e.getMessage());
+        }
+    }
+}
